1f77041ec1443b16ec9491ae44f45a0360238cc6
Abduraxim <toxirmalk@gmail.com>
2024-11-25 14:13:52 +0500

    Kichik bug tuzatildi

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ dotenv.config()
 
 
 process.on('uncaughtException', (err) => {
-    logger.error('Uncaught Exception', err)
+    console.error('Uncaught Exception', err)
     process.exit(1)
 })
 
 process.on("unhandledRejection", (reason, promise) => {
-    logger.error('Unhandled Rejection at', promise, 'reason', reason)
+    console.error('Unhandled Rejection at', promise, 'reason', reason)
     process.exit(1)
 })
 
