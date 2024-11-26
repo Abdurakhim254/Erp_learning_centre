10ce2b2162fb61b98277a8423022e75955ccce4e
Abduraxim <toxirmalk@gmail.com>
2024-11-26 15:46:28 +0500

    Qo'shimcha

--- add.js
+++ add.js
@@ -1,7 +1,16 @@
+console.log("\n\nShallow copy\n")
+let a={name:"Azim",age:25}
+let b=JSON.parse(JSON.stringify(a))
+console.log(a,b)
+a.name="Said"
+a.age=77
+console.log(a,b)
 
-for (let  i = 0; i <10;i++){
-    //comment 
+console.log("\nDeep copy\n")
 
-    console.log(i)
-
-}
+let c={name:"Azim",age:25}
+let d=c
+console.log(c,d)
+c.name="Akrom"
+c.age=11
+console.log(c,d)
--- src/app.js
+++ src/app.js
@@ -1,31 +1,48 @@
-import express from 'express'
-import {mainRouter} from "./routes/main.js"
-import {createAlltables} from "../src/schema/index.js"
-import helmet from 'helmet'
-import { rateLimit } from 'express-rate-limit'
+import express from "express";
+import { mainRouter } from "./routes/main.js";
+import { createAlltables } from "../src/schema/index.js";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
+import DeviceDetector from "node-device-detector";
 
-export const app=express()
+export const app = express();
 
+const detector = new DeviceDetector();
 
-const limitter=rateLimit({
-    windowMs:15*60*1000,
-    limit:20,
+const limitter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 20,
+});
 
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+                                  
+app.use(helmet());
+app.use(limitter);
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.get("/api/v1/setup", async (req, res) => {
+  try {
+    await createAlltables();
+    res.status(200).send("Table yaratildi");
+  } catch (error) {
+    res.status(400).send("Table yaratishda xatolik");
+  }
+});
 
-app.use(helmet())
-app.use(limitter)
+app.use("/api/v1", mainRouter);
 
-app.get("/api/v1/setup",async(req,res)=>{
-    try {
-        await createAlltables()
-        res.status(200).send("Table yaratildi")
-    } catch (error) {
-        res.status(400).send("Table yaratishda xatolik")
-    }
-})
+app.use((req, res) => {
+  const ip = req.ip;
+  const ips = req.ips;
+  const host = req.host;
+  const hostname = req.hostname;
+  const xhr = req.xhr;
 
-app.use("/api/v1",mainRouter)
+  const userAgent = req.headers["user-agent"];
+  const result = detector.detect(userAgent);
+
+  // console.log('result-parse',result)
+  console.log({ ...result, ip, ips, host, hostname, xhr });
+
+  res.send(result);
+});
